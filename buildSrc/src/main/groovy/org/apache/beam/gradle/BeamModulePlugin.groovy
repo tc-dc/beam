8c98b3edcc545079599beb35227345f50e5974b8
steve <sniemitz@twitter.com>
2019-02-11 09:51:11 -0500

    Bumped to 2.13.0.twttr1

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -306,10 +306,10 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.13.0'
-    if (!isRelease(project)) {
-      project.version += '-SNAPSHOT'
-    }
+    project.version = '2.13.0.twttr1'
+    //if (!isRelease(project)) {
+    //  project.version += '-SNAPSHOT'
+    //}
 
     project.apply plugin: 'org.apache.beam.jenkins'
 
